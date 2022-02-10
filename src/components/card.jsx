de35823389836872b9134637dc1333af67e8aedc
Adeola Bamigboye <Adeola.Bamigboye@prisma-capacity.eu>
2022-02-10 15:11:52 +0100

    add homepage

---
+++ src/components/card.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+export const Card = styled.div`
+@import url('https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+  padding: 1.5rem;
+  border: 0.1rem solid black;
+  background-color: white;
+  box-shadow: 0 0.1rem 0.1rem rgba(0, 0, 0, 0.05);
+`;
+
+export default Card;
